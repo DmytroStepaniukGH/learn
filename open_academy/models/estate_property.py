8cdb1f43eefb15d0a67de77487d570e3dcb2a49d
Dmytro Stepaniuk <dimastepaniukk@gmail.com>
2025-02-03 18:41:22 +0200

    Chapter 6

---
+++ open_academy/models/estate_property.py
@@ -0,0 +1,48 @@
+from odoo import models, fields
+from datetime import timedelta
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today() + timedelta(days=90)
+    )
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+
+    garden_orientation = fields.Selection(
+        [
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ],
+        string='Garden Orientation'
+    )
+
+    active = fields.Boolean("Active", default=True)
+
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="new",
+    )
